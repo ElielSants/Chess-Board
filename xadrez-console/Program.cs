59b4858c1f40ff123ec3e53874c21c8e28aa4c0f
Eliel Santos <elieltsants@gmail.com>
2021-01-28 09:48:59 -0300

    Tratando erros com exececao personalizada

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -8,17 +8,22 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Position P;
+            try{
 
             Board board = new Board(8, 8);
 
             board.putPiece(new Rook(board, Color.Black), new Position(0, 0));
-            board.putPiece(new Rook(board, Color.Black), new Position(1, 3));
-            board.putPiece(new King(board, Color.Black), new Position(2, 4));
+            board.putPiece(new Rook(board, Color.Black), new Position(1, 9));
+            board.putPiece(new King(board, Color.Black), new Position(0, 2));
 
 
             Screen.PrintBoard(board);
 
+            
+            }catch(BoardException e)
+            {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadLine();
         }
     }
--- xadrez-console/board/Board.cs
+++ xadrez-console/board/Board.cs
@@ -2,26 +2,57 @@ namespace board
 {
     public class Board
     {
-        public int lines { get; set; }
-        public int columns { get; set; }
-        private Piece[,] pieces;
-
-        public Board(int lines, int columns)
+        public Board(int lines, int columns) 
         {
             this.lines = lines;
             this.columns = columns;
             pieces = new Piece[lines, columns];
-        } 
+               
+        }
+        public int lines { get; set; }
+        public int columns { get; set; }
+        private Piece[,] pieces;
 
         public Piece Piece(int line, int column)
         {
             return pieces[line, column];
         }
 
+        public Piece Piece(Position pos)
+        {
+            return pieces[pos.line, pos.column]; ;
+        }
+
+        public bool existPiece(Position pos)
+        {
+            validatePosition(pos);
+            return Piece(pos) != null;
+        }
+
         public void putPiece(Piece p, Position pos)
         {
+            if(existPiece(pos)){
+                throw new BoardException("Já existe uma peça nessa posição");
+            }
             pieces[pos.line, pos.column] = p;
             p.position = pos;
+        }
+
+        public bool validPosition(Position pos)
+        {
+            if (pos.line < 0 || pos.line >= lines || pos.column < 0 || pos.column >= columns)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void validatePosition(Position pos)
+        {
+            if (!validPosition(pos))
+            {
+                throw new BoardException("Posi��o inv�lida!");
+            }
         }
     }
 }
